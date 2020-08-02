006b146a4d1de973c7c865ece5debaae4fbf184f
Huntly Cameron <huntly.cameron@gmail.com>
2020-08-02 17:57:32 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [timestamp, setTimestamp] = useState(new Date());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTimestamp(new Date());
+    }, 500);
+    return () => clearInterval(interval);
+  }, []);
+
+  const minuteToDeg = (min) => min*6;
+  const hourToDeg = (hour) => {
+    if(hour > 12){
+      hour -= 12;
+    }
+    return hour*30;
+  }
+
+  const hourDeg = hourToDeg(timestamp.getHours());
+  const minDeg = minuteToDeg(timestamp.getMinutes());
+
+  const hourStyle = {
+    transform: `rotate(${hourDeg}deg)`
+  };
+
+  const minStyle = {
+    transform: `rotate(${minDeg}deg)`
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img style={minStyle} className="back" src={process.env.PUBLIC_URL + '/clock-back.png'} alt=""/>
+      <img style={hourStyle} className="middle" src={process.env.PUBLIC_URL + '/clock-middle.png'} alt=""/>
+      <img className="marker" src={process.env.PUBLIC_URL + '/marker.png'} alt=""/>
     </div>
   );
 }
