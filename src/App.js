aa7d64e1f17aecc37c509063158d6b64149dd311
Huntly Cameron <huntly.cameron@gmail.com>
2020-08-02 19:25:28 +0100

    cleanp and day/night change

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 
 function App() {
   const [timestamp, setTimestamp] = useState(new Date());
+  const [dayOrNight, setDayOrNight] = useState();
+  const [isShowingDebugClock, setDebugClock] = useState(false);
+
 
   useEffect(() => {
     const interval = setInterval(() => {
       setTimestamp(new Date());
-    }, 500);
+    }, 200);
     return () => clearInterval(interval);
   }, []);
+
+
+  useEffect(() => {
+      if(timestamp.getHours() > 18){
+        setDayOrNight('night');
+      }else{
+        setDayOrNight('day');
+      }
+
+  }, [timestamp])
+
 
   const minuteToDeg = (min) => min*6;
   const hourToDeg = (hour) => {
@@ -30,11 +44,19 @@ function App() {
     transform: `rotate(${minDeg}deg)`
   };
 
+  const showDebugClock = () => {
+    setDebugClock(true);
+    setTimeout(() => { setDebugClock(false) }, 2000);
+  }
+
   return (
-    <div className="App">
+    <div onClick={showDebugClock} className="App">
       <img style={minStyle} className="back" src={process.env.PUBLIC_URL + '/clock-back.png'} alt=""/>
-      <img style={hourStyle} className="middle" src={process.env.PUBLIC_URL + '/clock-middle.png'} alt=""/>
+      <img style={hourStyle} className="middle" src={process.env.PUBLIC_URL + `/clock-middle-${dayOrNight}.png`} alt=""/>
       <img className="marker" src={process.env.PUBLIC_URL + '/marker.png'} alt=""/>
+      {isShowingDebugClock &&
+        <time>{timestamp.toLocaleTimeString()}</time>
+      }
     </div>
   );
 }
